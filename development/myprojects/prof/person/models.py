64d148dcd9e08c82f1571b7d7a4b6180f1096857
aghee <mutembeiagnes@gmail.com>
2018-05-12 00:08:59 +0300

    pdetails model added

--- development/myprojects/prof/person/models.py
+++ development/myprojects/prof/person/models.py
@@ -4,6 +4,18 @@ class edu(models.Model):
     sec=models.CharField(max_length=100)
     almamater=models.CharField(max_length=100,verbose_name='AlmaMater')
     period=models.DateTimeField()
+    def __str__(self):
+        return self.primary
+class pdetails(models.Model):
+    name=models.CharField(max_length=100)
+    DoB=models.DateTimeField()
+    residence=models.CharField(max_length=100)
+    maritalstatus=models.CharField(max_length=100)
+    hobbies=models.TextField(blank=False,null=False)
+    def __str__(self):
+        return self.name
+
+
 
 
 # Create your models here.
